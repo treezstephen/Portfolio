29fc890a4b33483747a5094f5a95989d1dcb9fec
scheung1206 <scheung1206@gmail.com>
2020-03-22 21:54:31 -0700

    Set up linters

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'arca',
+        'autofix',
+        'import',
+        'sort-keys-fix',
+        'prettier',
+    ],
+    'rules': {
+        'arca/import-align': 2,
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'always-multiline',
+            'exports': 'always-multiline',
+            'functions': 'never',
+        }],
+        'import/order': ['error'],
+        'sort-keys-fix/sort-keys-fix': ['error', 'asc', {'caseSensitive': false, 'natural': true}],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'key-spacing' : [
+            'warn',
+            {
+                align: 'value'
+            }
+        ],
+    }
+};
