2344ffa3556f59c8e77f396baa39c90cf6fedd49
treezstephen <stephen@treez.io>
2020-06-19 00:15:28 -0700

    Begin experiment with colors

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,8 +10,7 @@ import {
 } from 'react-icons/fa';
 import Resume    from '../../resources/files/resume_stephen_cheung.pdf';
 
-const ABOUT_TEXT = 'My name is Stephen Cheung, Software Engineer at Treez.io. Coding for curiosity is a motto I have adopted in this evolving world of software development. Whether for work or for a hobby, adding new technology to my arsenal is always a treat. Let\'s work on the next big project together. Connect with me with my channels below.';
-const ABOUT_SUBTITLE_TEXT = 'Full-stack developer based in the Bay Area';
+const ABOUT_TEXT = 'My name is Stephen Cheung, Software Engineer at Treez.io. I am a Full-stack developer based in the Bay Area. Coding for curiosity is a motto I have adopted in this evolving world of software development. Whether for work or for a hobby, adding new technology to my arsenal is always a treat. Let\'s work on the next big project together. Connect with me with my channels below.';
 
 export const About: FunctionComponent = () => {
     return (
@@ -20,9 +19,6 @@ export const About: FunctionComponent = () => {
                 <div className="bio-header">
                     <div className='bio-title'>
                         Software Engineer
-                    </div>
-                    <div className="subtitle">
-                        { ABOUT_SUBTITLE_TEXT }
                     </div>
                 </div>
                 <div className='biography'>
